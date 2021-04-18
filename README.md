be478ce661b2d901b6803959d9334db42c6811d1
UtkarshDhyani <77673410+UtkarshDhyani@users.noreply.github.com>
2021-04-18 10:49:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Multiplayer-game
+# synchronousBallMovement
+Ball moving synchronously
---
+++ sketch.js
@@ -0,0 +1,21 @@
+var form,game,player,database,playerCount,canvas;
+var gameState=0;
+
+function setup(){
+  database = firebase.database();
+  
+ canvas= createCanvas(400,400);
+game=new Game
+game.getState()
+game.start()
+ 
+}
+
+function draw(){
+  background("white");
+  
+  
+  
+}
+
+
